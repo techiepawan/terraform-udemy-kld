b8fe623d91bde929079f4a55bc97cb00e6556579
techiepawan <pawanyadav382@gmail.com>
2024-12-21 22:09:40 +0530

    Adding cleaned 05-01-MetaArgument-Count-For-Loops-Lists-Maps folder

--- var-ec2-providers/c2-ec2instances.tf
+++ var-ec2-providers/c2-ec2instances.tf
@@ -1,8 +1,11 @@
 resource "aws_instance" "myec2vm" {
-    ami = "ami-0aa117785d1c1bfe5"
+    ami = data.aws_ami.amzlinux2.id
     instance_type = var.aws_instance_type
+    #instance_type = var.instance_type_list[1]  # For List
+    #nstance_type = var.instance_type_map["prod"]  # For Map
     key_name = var.instance_key_name
     user_data = file("${path.module}/app1-install.sh")
+    vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
     tags = {
       "Name" = "demo-server"
     }
